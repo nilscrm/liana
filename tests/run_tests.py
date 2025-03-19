ea98b9670072a371e05428e2ab59b2ef4d153a37
Nils Cremer <38761406+nilscrm@users.noreply.github.com>
2025-03-19 11:54:31 +0100

    Add test for unbounded LP (#9)

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -4,11 +4,6 @@ import os
 num_tests = 0
 num_passed = 0
 
-expected_output = {
-    "lp1.vi": (18, [2, 3]),
-    "lp2.vi": (23, [2, 3]),
-}
-
 for file in os.listdir("tests/linear_programs"):
     print(f"Running test {file}: ", end="")
     num_tests += 1
@@ -19,19 +14,23 @@ for file in os.listdir("tests/linear_programs"):
             stderr=subprocess.PIPE,
             text=True,
         )
-
-        sol_val, sol = result.stdout.split(",", maxsplit=1)
-        sol_val = float(sol_val.strip())
-        sol = list(float(x) for x in sol.strip().strip("[]").split(","))
-        expected_sol_val, expected_sol = expected_output[file]
-        if sol_val == expected_sol_val and sol == expected_sol:
-            print("PASSED")
-            num_passed += 1
-        else:
+        try:
+            file_stem = file.split(".")[0]
+            with open(f"tests/outputs/{file_stem}.out", "r") as f:
+                expected_output = f.read()
+                if result.stdout == expected_output:
+                    print("PASSED")
+                    num_passed += 1
+                else:
+                    print("FAILED")
+                    print(f"Expected: {expected_output}")
+                    print(f"Got: {result.stdout}")
+                    print(result.stderr)
+        except FileNotFoundError:
             print("FAILED")
-            print(f"Expected: {expected_sol_val}, {expected_sol}")
-            print(f"Got: {sol_val}, {sol}")
-            print(result.stderr)
+            with open(f"tests/outputs/{file_stem}.out", "w") as f:
+                f.write(result.stdout)
+            print("Created output file")
     except Exception as e:
         print("FAILED")
         print(e)
